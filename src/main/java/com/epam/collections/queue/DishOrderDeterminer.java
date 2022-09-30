8116aa3fbe499975b20d8742454e896716acb888
kachanau <BsuHelp@tut.by>
2022-09-30 23:09:38 +0200

    queues

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,24 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1 ;i<numberOfDishes;i++){
+            queue.add(i);
+        }
+        Iterator<Integer> itr = queue.iterator();
+        while (itr.hasNext()) {
+            for(int i=1;i<everyDishNumberToEat;i++){
+                if(itr.hasNext()) {
+                    itr.next();
+                }
+            }
+            list.add(queue.poll());
+        }
+
+        return list;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,39 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
+
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> queue = new PriorityQueue<>(new The_Comparator());
+       // PriorityQueue<String> queue1 = new PriorityQueue<>(new The_Comparator());
+
+        if (!(firstList.isEmpty() && secondList.isEmpty())) {
+            queue.addAll(firstList);
+            queue.addAll(secondList);
+        }
+      //  String str1[] = queue1.toArray(new String[0]);
+
+      //  Collections.reverse(Arrays.asList(str1));
+
+
+    //    queue1.addAll(queue);
+
+
+return queue;
+    }
+
+
+}
+
+
+class The_Comparator implements Comparator<String> {
+    public int compare(String str1, String str2)
+    {
+        String first_Str;
+        String second_Str;
+        first_Str = str1;
+        second_Str = str2;
+        return second_Str.compareTo(first_Str);
     }
 }
